a02a31b2e080eb76742b4aeb199a99323966a83e
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-14 21:41:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,18 @@
+def oxford_comma(array, delimiter = "")
+  string = ""
+  last_index = array.length - 1
+  array.each_with_index do |elem, index|
+    string << elem
+    string << delimiter unless index == last_index
+  end  
+  string
+end
 
+puts oxford_comma(array, ",")
 
 
   def oxford_comma(array)
-    if array.length == 1
+    if array.length == 1 
       return array[0]
     elsif array.length == 2  
       array.join(" and ")
